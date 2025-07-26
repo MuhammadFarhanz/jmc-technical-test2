3ca54cd4699329c15d5565498e35ca562ac39c73
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2025-07-27 00:05:07 +0700

    first commit

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,27 +1,34 @@
-import '../css/app.css';
-import './bootstrap';
+import "../css/app.css";
+import "./bootstrap";
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, DefineComponent, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createApp, DefineComponent, h } from "vue";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import { QueryClient, VueQueryPlugin } from "@tanstack/vue-query";
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
+            import.meta.glob<DefineComponent>("./Pages/**/*.vue")
         ),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
+        const queryClient = new QueryClient();
+
+        const vueApp = createApp({
+            render: () => h(App, props),
+        });
+
+        vueApp.use(plugin);
+        vueApp.use(ZiggyVue);
+        vueApp.use(VueQueryPlugin, { queryClient });
+
+        vueApp.mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
